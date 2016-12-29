09424b4378d38ffd848555a60bafafffa7e036b7
Rose Yu <qiyu@usc.edu>
2016-12-28 16:40:35 -0800

    Added lstm ptb demo

--- experiments/ptb/TensorBasicLSTMCell.py
+++ experiments/ptb/TensorBasicLSTMCell.py
@@ -1,13 +1,7 @@
 
 # coding: utf-8
 
-# ## LSTM cell 
-# $$ i, j, f, o = Wx + Uh + b \\
-#    [i,j,f,o] = [W,U,1][x,h,b]^\top$$
-#   i = input_gate, j = new_input, f = forget_gate, o = output_gate
-# 
-
-# In[1]:
+# In[2]:
 
 from __future__ import absolute_import
 from __future__ import division
@@ -171,80 +165,4 @@ def _linear(args, output_size, bias, bias_start=0.0, scope=None):
                 bias_start, dtype=dtype))
     return res + bias_term
 
-
-
-if __name__ == '__main__':
-    np.random.seed(1)
-    # the size of the hidden state for the lstm (notice the lstm uses 2x of this amount so actually lstm will have state of size 2)
-    size = 12
-    # 2 different sequences total
-    batch_size= 2
-    # the maximum steps for both sequences is 10
-    n_steps = 10
-    # each element of the sequence has dimension of 5
-    seq_width = 2
-
-    # the first input is to be stopped at 4 steps, the second at 6 steps
-    e_stop = np.array([4,6])
-    
-    # factorize the inp
-    inp_modes = {}
-    out_modes = {}
-    mat_ranks = {}
-    # input weights
-    inp_modes['x'] = np.array([1, 2, 1, 1], dtype='int32') # product as seq_width
-    out_modes['x'] = np.array([1, 4, 3, 1], dtype='int32') # product as num_units (size)
-    mat_ranks['x'] = 2
-    #mat_ranks['x'] = np.array([1, 2, 2, 2, 1], dtype='int32')
-    # hidden state weights
-    inp_modes['h'] = np.array([1, 4, 3, 1], dtype='int32') # seq_width
-    out_modes['h'] = np.array([1, 4, 3, 1], dtype='int32') # 4 * num_units
-    mat_ranks['h'] = 2
-
-    #mat_ranks['h'] = np.array([1, 2, 2, 2, 1], dtype='int32') 
-    
-    
-    initializer = tf.random_uniform_initializer(-1,1)
-
-    # the sequences, has n steps of maximum size
-    seq_input = tf.placeholder(tf.float32, [n_steps, batch_size, seq_width], name="placeholder/seqs")
-    # what timesteps we want to stop at, notice it's different for each batch hence dimension of [batch]
-    early_stop = tf.placeholder(tf.int32, [batch_size], name="placeholder/stops" )
-
-    # inputs for rnn needs to be a list, each item being a timestep.
-    # we need to split our input into each timestep, and reshape it because split keeps dims by default
-    # input = [n_steps, batch_size, seq_width]
-    inputs = [tf.reshape(i, (batch_size, seq_width)) for i in tf.split(0, n_steps, seq_input)]
-    
-    """Shape checker"""
-#     init = tf.initialize_all_variables()
-#     sess = tf.InteractiveSession()
-#     sess.run(init)
-#     print(sess.run(tf.shape(inputs), feed_dict = {seq_input: np.ones((n_steps, batch_size, seq_width)) }))
-#     sess.close()
-    
-    cell = TensorBasicLSTMCell(size, inp_modes=inp_modes, out_modes=out_modes, mat_ranks=mat_ranks, 
-                          input_size = seq_width , initializer=initializer)        
-        
-    initial_state = cell.zero_state(batch_size, tf.float32)
-
-    # ========= This is the most important part ==========
-    # output will be of length 4 and 6
-    # the state is the final state at termination (stopped at step 4 and 6)
-    outputs, state = tf.nn.rnn(cell, inputs, initial_state=initial_state, sequence_length=early_stop)
-
-    # usual crap
-    iop = tf.initialize_all_variables()
-    session = tf.Session()
-    session.run(iop)
-    feed = {early_stop:e_stop, seq_input:np.random.rand(n_steps, batch_size, seq_width).astype('float32')}
-
-    print("outputs, should be 2 things one of length 4 and other of 6")
-    outs = session.run(outputs, feed_dict=feed)
-    for xx in outs:
-        print(xx)
-
-    print("states, 2 things total both of size 2, which is the size of the hidden state")
-    st = session.run(state, feed_dict=feed)
-    print(st)
 

4a18950261fb79a5db04f948924ff3debc51a989
StephanZheng <st.t.zheng@gmail.com>
2017-02-23 19:06:12 -0800

    Add timestamp to runner.

--- experiments/lorenz_error/run_exps.sh
+++ experiments/lorenz_error/run_exps.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+timestamp() {
+  date +"%T"
+}
+datestamp() {
+  date +"%D"
+}
+
+t=$(timestamp)
+t="$(echo ${t} | tr ':' '-')"
+
+d=$(datestamp)
+d=$(echo ${d} | tr '/' '-')
+
+start_time="$d-$t"
+
 use_error=--use_error_prop
 d=/cs/ml/datasets/stephan/tensorcompress/lorenz_series.pkl
+exp=lorenz_error_exp
 
-python seq_train.py --data_path=$d $use_error
-python seq_train_lstm.py --data_path=$d $use_error
-python seq_train_matrix.py --data_path=$d $use_error
-python seq_train_tensor.py --data_path=$d $use_error
+
+base_dir=/tmp/tensorcompress/log/$exp/$start_time
+
+save_path=$base_dir/basic_rnn
+python seq_train.py --data_path=$d --save_path=$save_path $use_error
+
+save_path=$base_dir/basic_lstm
+python seq_train_lstm.py --data_path=$d --save_path=$save_path $use_error
+
+save_path=$base_dir/matrix_rnn
+python seq_train_matrix.py --data_path=$d --save_path=$save_path $use_error
+
+save_path=$base_dir/tt_rnn
+python seq_train_tensor.py --data_path=$d --save_path=$save_path $use_error

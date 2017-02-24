dae9bd7b0fbd3b86b835fdd06fb524be92bfba48
Stephan Zheng <stephan@caltech.edu>
2017-02-23 19:15:43 -0800

    Runner fix.

--- experiments/lorenz_error/run_exps.sh
+++ experiments/lorenz_error/run_exps.sh
@@ -16,20 +16,22 @@ d=$(echo ${d} | tr '/' '-')
 start_time="$d-$t"
 
 use_error=--use_error_prop
-d=/cs/ml/datasets/stephan/tensorcompress/lorenz_series.pkl
+data_path=/cs/ml/datasets/stephan/tensorcompress/lorenz_series.pkl
 exp=lorenz_error_exp
 
 
 base_dir=/tmp/tensorcompress/log/$exp/$start_time
 
+echo $base_dir
+
 save_path=$base_dir/basic_rnn
-python seq_train.py --data_path=$d --save_path=$save_path $use_error
+python seq_train.py --data_path=$data_path --save_path=$save_path $use_error
 
 save_path=$base_dir/basic_lstm
-python seq_train_lstm.py --data_path=$d --save_path=$save_path $use_error
+python seq_train_lstm.py --data_path=$data_path --save_path=$save_path $use_error
 
 save_path=$base_dir/matrix_rnn
-python seq_train_matrix.py --data_path=$d --save_path=$save_path $use_error
+python seq_train_matrix.py --data_path=$data_path --save_path=$save_path $use_error
 
 save_path=$base_dir/tt_rnn
-python seq_train_tensor.py --data_path=$d --save_path=$save_path $use_error
+python seq_train_tensor.py --data_path=$data_path --save_path=$save_path $use_error

96d22b29ce05ada55125fb1264db1ec37a1866a0
StephanZheng <st.t.zheng@gmail.com>
2017-02-24 13:45:19 -0800

    rnn

--- experiments/ts_error/run_exps.sh
+++ experiments/ts_error/run_exps.sh
@@ -25,20 +25,47 @@ exp=ts_error_exp
 num_steps=50
 hidden_size=128
 
-base_dir=/tmp/tensorcompress/log/$exp/rollout_$num_steps$use_error_path/$start_time
+# With error-prop
+
+use_error= #--use_error_prop
+use_error_path=/no_feed_prev
+base_dir=/tmp/tensorcompress/log/$start_time/$exp/rollout_$num_steps$use_error_path
 echo $base_dir
 
 save_path=$base_dir/basic_rnn
 python seq_train.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
 
-save_path=$base_dir/basic_lstm
-python seq_train_lstm.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+# save_path=$base_dir/basic_lstm
+# python seq_train_lstm.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
 
-save_path=$base_dir/matrix_rnn
-python seq_train_matrix.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+# save_path=$base_dir/matrix_rnn
+# python seq_train_matrix.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
 
 # save_path=$base_dir/tt_rnn
 # python seq_train_tensor.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
 
-save_path=$base_dir/einsum_tt_rnn
-python seq_train_tensor_einsum.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+# save_path=$base_dir/einsum_tt_rnn
+# python seq_train_tensor_einsum.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+
+
+# Without error-prop
+
+use_error=--use_error_prop
+use_error_path=/feed_prev
+base_dir=/tmp/tensorcompress/log/$start_time/$exp/rollout_$num_steps$use_error_path
+echo $base_dir
+
+save_path=$base_dir/basic_rnn
+python seq_train.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+
+# save_path=$base_dir/basic_lstm
+# python seq_train_lstm.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+
+# save_path=$base_dir/matrix_rnn
+# python seq_train_matrix.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+
+# save_path=$base_dir/tt_rnn
+# python seq_train_tensor.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error
+
+# save_path=$base_dir/einsum_tt_rnn
+# python seq_train_tensor_einsum.py --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_steps=$num_steps $use_error

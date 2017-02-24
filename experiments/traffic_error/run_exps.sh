d8e2ea621ece5801f218b469664e86413978727a
StephanZheng <st.t.zheng@gmail.com>
2017-02-24 13:06:38 -0800

    no error prop

--- experiments/traffic_error/run_exps.sh
+++ experiments/traffic_error/run_exps.sh
@@ -15,8 +15,8 @@ d=$(echo ${d} | tr '/' '-')
 
 start_time="$d-$t"
 
-use_error=--use_error_prop
-use_error_path=/feed_prev
+use_error= #--use_error_prop
+use_error_path=/no_feed_prev
 data_path=/cs/ml/datasets/stephan/tensorcompress/traffic_9sensors.pkl
 # chaotic_ts_mat.pkl  chaotic_ts.pkl  lorenz_series_mat.pkl  lorenz_series.pkl  traffic_9sensors.pkl  ushcn_CA.pkl
 

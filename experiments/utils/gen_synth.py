8e2fd6dcbf3b763fad89597be8e04cb05a2892d6
Rose Yu <qiyu@usc.edu>
2017-05-08 13:44:38 -0700

    Updated gen_synth.py

--- experiments/utils/gen_synth.py
+++ experiments/utils/gen_synth.py
@@ -21,8 +21,7 @@ def lorenz(x, y, z, s=10, r=28, b=2.667):
     z_dot = x*y - b*z
     return x_dot, y_dot, z_dot
 
-
-def gen_lorenz_series(x0, y0, z0, num_steps):
+def gen_lorenz_series(x0, y0, z0, num_steps, num_freq):
     dt = 0.01
     stepCnt = num_steps
 
@@ -37,29 +36,40 @@ def gen_lorenz_series(x0, y0, z0, num_steps):
     ys[0] = y0
     zs[0] = z0
 
+    xss = np.empty((stepCnt//num_freq,))
+    yss = np.empty((stepCnt//num_freq,))
+    zss = np.empty((stepCnt//num_freq,))
     # Stepping through "time".
+    j = 0
     for i in range(stepCnt-1):
         # Derivatives of the X, Y, Z state
+        if i%num_freq ==0:
+            xss[j] = xs[i]
+            yss[j] = ys[i]
+            zss[j] = zs[i]
+            j += 1
         x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
         xs[i + 1] = xs[i] + (x_dot * dt)
         ys[i + 1] = ys[i] + (y_dot * dt)
         zs[i + 1] = zs[i] + (z_dot * dt)
 
     #save the sequence for training
-    lorenz_series = np.transpose(np.vstack((xs,ys,zs)))
+    lorenz_series = np.transpose(np.vstack((xss,yss,zss)))
     return lorenz_series
 
 def gen_lorenz_dataset(file_name="lorenz.pkl"):
     #define initial range
-    num_samples = 200
-    num_steps = int(1e2)
-    init_range = np.random.uniform(0.1,1.0,(num_samples,3))
+    num_samples = int(1e5)
+    num_freq = int(5)
+    num_steps = int(1e2)*num_freq
+    
+    init_range = np.random.uniform(-20,20,(num_samples,3))
    
-    lorenz_series_mat = np.ndarray((num_samples, num_steps, 3))
+    lorenz_series_mat = np.ndarray((num_samples, num_steps//num_freq, 3))
 
     for i in range(num_samples):
         x0,y0,z0 = init_range[i,:]
-        series = gen_lorenz_series(x0,y0,z0, num_steps )
+        series = gen_lorenz_series(x0,y0,z0, num_steps, num_freq )
         lorenz_series_mat[i,:,:] = series
                 
     pickle.dump(lorenz_series_mat, open(file_name,"wb")) 

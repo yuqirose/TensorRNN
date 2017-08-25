a3c6dabf88196bc6aaad06988f2db9d7f1ffb857
Rose Yu <qiyu@usc.edu>
2017-08-24 21:49:38 -0700

    Fixed the input bug for plstm

--- experiments/train/seq_train_plstm.py
+++ experiments/train/seq_train_plstm.py
@@ -20,7 +20,7 @@ logging = tf.logging
 flags.DEFINE_string(
     "model", "small",
     "A type of model. Possible options are: small, medium, large.")
-flags.DEFINE_string("data_path", "/Users/roseyu/Documents/Python/lorenz.pkl",
+flags.DEFINE_string("data_path", "/Users/roseyu/Documents/Python/data/lorenz.npy",
                     "Where the training/test data is stored.")
 flags.DEFINE_string("save_path", "/Users/roseyu/Documents/Python/lorenz/basic_lstm/",
                     "Model output directory.")

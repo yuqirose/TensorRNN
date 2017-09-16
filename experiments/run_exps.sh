68166efa49d5113efdf150260ff8838a28a56035
Rose Yu <qiyu@usc.edu>
2017-09-16 16:23:55 -0700

    added the model para in bash

--- experiments/run_exps.sh
+++ experiments/run_exps.sh
@@ -23,26 +23,27 @@ use_error=True #--use_error_prop
 # hidden_size=128
 burn_in_steps=5 # just for naming purposes
 num_steps=20
-num_test_steps=20
+num_test_steps=100
 
 
 for exp in lorenz #climate traffic 
 do 
-    data_path=/home/roseyu/data/tensorRNN/${exp}.npy
+data_path=/home/roseyu/data/tensorRNN/${exp}.npy
 
-        for hidden_size in 16
+    for hidden_size in 16
+    do
+        for learning_rate in 1e-2 
         do
-            for learning_rate in 1e-2 
-            do
             #for num_test_steps in 10 15 20 25 30 35 40 45 
             #do
-            save_path=/var/tmp/tensorRNN/log/$exp/$start_time/hz_$hidden_size/lr_$learning_rate/RNN/
+            for model in RNN
+            do
+            save_path=/var/tmp/tensorRNN/log/$exp/$start_time/hz_$hidden_size/lr_$learning_rate/$model/
             echo $save_path
-	    mkdir -p $save_path
-            python train.py --model=RNN --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_train_steps=$num_train_steps --num_test_steps=$num_test_steps --learning_rate=$learning_rate --use_error_prop=$use_error
-                  
-    
-    	    done
+            mkdir -p $save_path
+            python train.py --model=$model --data_path=$data_path --save_path=$save_path --hidden_size=$hidden_size --num_train_steps=$num_train_steps --num_test_steps=$num_test_steps --learning_rate=$learning_rate --use_error_prop=$use_error  
+            done
+	    done
     done
 done
 
